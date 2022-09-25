0d82a2c8228d3d339ee7daacfd18e310fb8fd6fe
Joao <pouljp@bol.com.br>
2022-09-25 19:26:41 -0300

    Aprendendo Estruturas de um site

---
+++ script.js
@@ -0,0 +1,14 @@
+function menuToogle(){
+    let menuArea = document.getElementById("menu-area");
+    
+    // if(menuArea.classList.contains('menu-opened') == true){
+    //     menuArea.classList.remove('menu-opened');
+    // }else {
+    //     menuArea.classList.add('menu-opened');
+    // }
+    if (menuArea.style.width == '200px'){
+        menuArea.style.width = '0px';
+    }else{
+        menuArea.style.width = '200px';
+    }
+}
